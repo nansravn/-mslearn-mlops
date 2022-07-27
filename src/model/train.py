b5812751c0808eb0397b5636429b86adf246292b
Renan Vilas Novas <vnovas.renan@gmail.com>
2022-07-27 14:20:44 -0300

    fix: linting

--- src/model/train.py
+++ src/model/train.py
@@ -49,7 +49,7 @@ def process_data(df):
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
-    model = LogisticRegression(C=1/reg_rate,
+    model = LogisticRegression(C=1 / reg_rate,
                                solver="liblinear").fit(X_train, y_train)
 
     # return model
